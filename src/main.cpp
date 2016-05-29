705cf6785c81bbd1671580e5f6d67d7fd5321129
megamilkx <gyrocopter3k@gmail.com>
2016-05-29 17:39:31 +0300

    Add switch to high priority at startup

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 
 int main()
 {
+    SetPriorityClass(GetModuleHandle(NULL), REALTIME_PRIORITY_CLASS);
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+
     if(!GameState::Init())
         return 0;
     
