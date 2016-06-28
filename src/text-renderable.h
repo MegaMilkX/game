7772381c6c83856b166e7a47c206260f79b2563e
megamilkx <gyrocopter3k@gmail.com>
2016-06-28 23:42:30 +0300

    Add alignment to TextRenderable

--- src/text-renderable.h
+++ src/text-renderable.h
@@ -13,82 +13,34 @@ class TextRenderable : public IRenderable
 		TextRenderable, IRenderable,
 		CONSTRUCTOR
 		(
-
 		)
 	)
 
 public:
-
-	void Font(ResHdl<GFXFont> fnt) { font = fnt; }
-
-	void Text(const std::string& txt, unsigned char size)
+	enum ALIGN
 	{
-		this->size = size;
-		text = txt;
-		mesh = font->MakeString(txt, size);
-	}
+		LEFT = 1,
+		RIGHT = 2,
+		TOP = 4,
+		BOTTOM = 8,
+		HCENTER = LEFT | RIGHT,
+		VCENTER = TOP | BOTTOM
+	};
+
+	void Font(const std::string& font_name) { string.Font(font_name); }
+	void Size(unsigned char size) { string.Size(size); }
+	void Align(int align) { string.Align(align); }
+	void Text(const std::string& txt) { string = txt; }
 
 	void Render()
 	{
 		global_matrixmodel = node->GetTransform();
-		font->Bind(size);
-		mesh.Render();
+		string.Render();
 	}
 
 protected:
-	unsigned char size;
-	std::string text;
-	GFXMesh mesh;
+	GFXString string;
 	GFXGlobal<mat4f> global_matrixmodel;
-	ResHdl<GFXFont> font;
 };
-/*
-DEF_ENTITY
-(
-	TextRenderable,
-	IRenderable,
-	CONSTRUCTOR
-	(
-		mesh = GFXMesh::Create(GFXMesh::DYNAMIC);
-	),
-	PUBLIC
-	(
-		void Text(const std::string& txt)
-		{ 
-			text = txt;
-			std::vector<Vertex> vertices;
-			std::vector<unsigned short> indices;
 
-			float advance_x;
-			float advance_y;
-
-			for (unsigned int i = 0; i < text.size(); ++i)
-			{
-
-
-				vertices +=
-				({
-					{ vec3f(advance_x + -0.5f, advance_y + -0.5f, 0.0f), vec3f(0.5f, 0.1f, 0.1f), vec2f(1.0f, 0.0f) },
-					{ vec3f(advance_x + 0.5f, advance_y + -0.5f, 0.0f), vec3f(0.5f, 0.3f, 0.1f), vec2f(0.0f, 0.0f) },
-					{ vec3f(advance_x + 0.5f, advance_y + 0.5f, 0.0f), vec3f(0.5f, 0.1f, 0.1f), vec2f(0.0f, 1.0f) },
-					{ vec3f(advance_x + -0.5f, advance_y + 0.5f, 0.0f), vec3f(0.5f, 0.3f, 0.1f), vec2f(1.0f, 1.0f) }
-				});
-
-				indices += ({ 0, 1, 2, 2, 0, 3 });
-
-				advance_x += 1.0f;
-				advance_y += 0.0f;
-			}
-
-			mesh.SetVertices(vertices);
-			mesh.SetIndices(indices);
-		}
-	),
-	PROTECTED
-	(
-		std::string text;
-		GFXMesh mesh;
-	)
-);
-*/
 #endif
